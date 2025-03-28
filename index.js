b5ab25d094ee7da77117a111397369d979c2fa38
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-03-28 09:28:13 +0100

    add PostgreSQL client and implement database connection for grades routes

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const pg = require('pg');
 const app = express();
+const client = new pg.Client({
+    user: 'user8', // Nazwa użytkownika bazy danych
+    host: '192.168.0.207', // Adres hosta bazy danych
+    database: 'user8_db', // Nazwa bazy danych
+    password: 'JBKVZDPK', // Hasło użytkownika bazy danych
+    port: 5432, // Port, na którym działa serwer PostgreSQL
+});
+
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 //app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
@@ -7,17 +16,27 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/grades/:student_id/', (req, res) => {
+app.get('/grades/:student_id/', async (req, res) => {
     const studentId = req.params.student_id;
     console.log("Student ID:", studentId);
+    const query = 'SELECT * FROM grades WHERE student_id = ' + studentId;
+    const result = await client.query(query);
+    console.log("Query Result:");
+    console.table(result.rows);
     res.end();
 });
-app.post('/grades/:student_id/', (req, res) => {
+app.post('/grades/:student_id/', async (req, res) => {
     const studentId = req.params.student_id;
     console.log("Student ID:", studentId);
     console.log("Request Body:", req.body);
     res.end();
 });
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3000, async () => {
+    await client.connect(); // Connect to the database
+    console.log('Connected to the database');
+    console.log('Server is running on port 3000');
 }); 
+app.on('close', async () => {
+    await client.end(); // Disconnect from the database when the server closes
+    console.log('Disconnected from the database');
+});
