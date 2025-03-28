24d7db3a58320fe6f73b50da8782fa42a5b3cd66
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-03-28 08:48:16 +0100

    add express dependency and implement basic server with routes

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON request bodies
+//app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/grades/:student_id/', (req, res) => {
+    const studentId = req.params.student_id;
+    console.log("Student ID:", studentId);
+});
+app.post('/grades/:student_id/', (req, res) => {
+    const studentId = req.params.student_id;
+    console.log("Student ID:", studentId);
+    console.log("Request Body:", req.body);
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+}); 
