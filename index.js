cde743c238aa12a4bf946a9ec27fb43abf6b67b1
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-04-04 08:52:12 +0200

    refactor grade routes to use student_id in URL and add input validation for POST requests

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/grades/:student_id/', async (req, res) => {
+// ze zbioru studentów
+// dla konkretnego studenta
+// jego oceny
+// zwracamy (GET) oceny studenta
+app.get('/students/:student_id/grades/', async (req, res) => {
     const studentId = req.params.student_id;
     console.log("Student ID:", studentId);
     const query = 'SELECT * FROM grades WHERE student_id = ' + studentId;
@@ -25,10 +29,34 @@ app.get('/grades/:student_id/', async (req, res) => {
     console.table(result.rows);
     res.send(result.rows);
 });
-app.post('/grades/:student_id/', async (req, res) => {
-    const studentId = req.params.student_id;
+// ze zbioru studentów
+// dla konkretnego studenta
+// jego oceny
+// dodajemy (POST) oceny studenta
+app.post('/students/:student_id/grades/', async (req, res) => {
+    const studentId = parseInt(req.params.student_id); // Parse student_id from URL parameter
     console.log("Student ID:", studentId);
     console.log("Request Body:", req.body);
+    // Validate request body
+    // jeżeli studentId jest null lub nie jest liczbą całkowitą lub nie jest liczbą skończoną
+    if(studentId === null || !Number.isInteger(studentId) || !isFinite(studentId)) {
+        //status 400 - Bad Request
+        res.status(400).send('Invalid student ID');
+        return;
+    }
+    if(req.body.course_id === null || !Number.isInteger(req.body.course_id) || !isFinite(req.body.course_id)) {
+        //status 400 - Bad Request
+        res.status(400).send('Invalid course ID');
+        return;
+    }
+    if(req.body.grade === null || !Number.isInteger(req.body.grade) || !isFinite(req.body.grade)) {
+        //status 400 - Bad Request
+        res.status(400).send('Invalid grade');
+        return;
+    }
+    const query = 'INSERT INTO grades (student_id, course_id, grade) VALUES ($1, $2, $3)';
+    const values = [studentId, req.body.course_id, req.body.grade];
+    await client.query(query, values);
     res.end();
 });
 app.listen(3000, async () => {
